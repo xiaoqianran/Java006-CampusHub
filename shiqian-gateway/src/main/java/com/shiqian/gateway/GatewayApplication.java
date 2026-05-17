ea410a6cd7067a94382fbfaedf2e1b5e412c7a4f
xiaoqianran <seachenxyt@gmail.com>
2026-05-17 10:25:53 +0000

    feat(gw): 实现 JWT 全局鉴权过滤器
    - 校验受保护路由的 Bearer Token
    
    - 放行登录注册健康检查和 OPTIONS 请求
    
    - 向下游服务透传用户身份请求头

--- shiqian-gateway/src/main/java/com/shiqian/gateway/GatewayApplication.java
+++ shiqian-gateway/src/main/java/com/shiqian/gateway/GatewayApplication.java
@@ -1,9 +1,12 @@
 package com.shiqian.gateway;
 
+import com.shiqian.common.security.JwtUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(JwtUtil.class)
 public class GatewayApplication {
 
     public static void main(String[] args) {
