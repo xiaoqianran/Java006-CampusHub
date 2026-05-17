edfdaf8cdecbe71a38103c677c7daaabb640db28
xiaoqianran <seachenxyt@gmail.com>
2026-05-17 14:14:42 +0000

    fix: 修复 RabbitMQ 消息序列化失败导致下载和审核接口 500 错误
    为 ResourceDownloadMessage 和 ResourceAuditMessage 实现 Serializable 接口并添加 serialVersionUID。
    在 RabbitMQConfig 中新增 Jackson2JsonMessageConverter Bean（注册 JavaTimeModule 支持 LocalDateTime），
    以及配置 RabbitTemplate 使用该转换器，解决默认转换器无法序列化 POJO 的问题。
    修复后下载/审核接口 MQ 消息发送成功，接口从 500 恢复为 200。

--- shiqian-resource/src/main/java/com/shiqian/resource/config/RabbitMQConfig.java
+++ shiqian-resource/src/main/java/com/shiqian/resource/config/RabbitMQConfig.java
@@ -4,6 +4,12 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -48,5 +54,24 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(resourceDownloadQueue())
                 .to(resourceTopicExchange())
                 .with(RESOURCE_DOWNLOAD_ROUTING_KEY);
+    }
+
+    /**
+     * 使用 Jackson 将对象序列化为 JSON，支持 LocalDateTime
+     */
+    @Bean
+    public MessageConverter jacksonMessageConverter() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        // 序列化时带上类型信息，consumer 也能正确反序列化
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter(mapper);
+        return converter;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jacksonMessageConverter());
+        return template;
     }
 }
--- shiqian-resource/src/main/java/com/shiqian/resource/dto/ResourceDownloadMessage.java
+++ shiqian-resource/src/main/java/com/shiqian/resource/dto/ResourceDownloadMessage.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
@@ -12,7 +13,9 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ResourceDownloadMessage {
+public class ResourceDownloadMessage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private Long resourceId;
 
